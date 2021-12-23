2da8a5fe21e64c64c1026ca9feddcf596e5287fc
Deb Kumar Mishra <47305904+Debkumar49@users.noreply.github.com>
2021-12-23 20:27:58 +0530

    Second Problem from list
    Finds out the ASCII value of the an alphabet.

---
+++ Solutions(C)/1. Simple Questions/ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    char c1;
+
+    printf("Enter the character to see ASCII value : ");
+    scanf("%c",&c1);
+    printf("The ASCII value of %c : %d\n",c1,c1);
+
+}
