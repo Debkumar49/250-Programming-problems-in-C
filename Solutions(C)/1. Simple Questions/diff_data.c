c29803055ff60180befef284a423ffc29b0bb652
Deb Kumar Mishra <47305904+Debkumar49@users.noreply.github.com>
2022-01-03 22:05:45 +0530

    Size of different DataTypes
    This program shows the size of different DataTypes along with example.

---
+++ Solutions(C)/1. Simple Questions/diff_data.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdbool.h>
+main()
+{
+int a;
+float b;
+char c;
+bool d;
+double e;
+long double f;
+short g;
+long long int h;
+
+
+printf("Size of int : %d\n",sizeof(a));
+printf("Size of float : %d\n",sizeof(b));
+printf("Size of char : %d\n",sizeof(c));
+printf("size of bool : %d\n",sizeof(d));
+printf("Size of double : %d\n",sizeof(e));
+printf("size of long double : %d\n",sizeof(f));
+printf("size of short : %d\n",sizeof(g));
+printf("size of long long int : %d\n",sizeof(h));
+
+}
